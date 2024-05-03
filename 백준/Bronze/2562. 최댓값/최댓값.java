f7f78a2c044b4fd1a4ccd6cb767e92ee196b1782
cussle <109949453+cussle@users.noreply.github.com>
2024-05-03 23:52:25 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        int max = 0, index = 0;
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            if (arr[i] > max) {
+                max = arr[i];
+                index = i + 1;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(index);
+    }
+}
