630f63e8a3bc1b663dee955cdca13eaa93eb07db
cussle <109949453+cussle@users.noreply.github.com>
2024-05-05 00:50:30 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 696 ms, Memory: 52416 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -9,6 +9,7 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int N = Integer.parseInt((br.readLine()));
         int[][] arr = new int[N][2];
@@ -29,7 +30,8 @@ public class Main {
         });
 
         for (int[] temp : arr) {
-            System.out.println(temp[0] + " " + temp[1]);
+            sb.append(temp[0]).append(" ").append(temp[1]).append("\n");
         }
+        System.out.println(sb);
     }
 }
