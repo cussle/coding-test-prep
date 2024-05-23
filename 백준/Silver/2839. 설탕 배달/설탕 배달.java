d779bb13ac0df450411bc9580d34f6c6ec46495d
cussle <109949453+cussle@users.noreply.github.com>
2024-05-23 23:05:19 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        int count = 0;
+        while (N >= 0) {
+            if (N % 5 == 0) {
+                System.out.println(N / 5 + count);
+                break;
+            }
+
+            N -= 3;
+            count++;
+
+            if (N < 0) {
+                System.out.println(-1);
+                break;
+            }
+        }
+    }
+}
