4fd79b195618d7721f9b52e7a208c8f0fa208b7a
cussle <109949453+cussle@users.noreply.github.com>
2025-10-16 16:27:08 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.05 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/README.md
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 정수를 나선형으로 배치하기 - 181832 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181832?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181832) 
 
 ### 성능 요약
 
-메모리: 77 MB, 시간: 0.03 ms
+메모리: 80.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 21일 16:45:22
+2025년 10월 16일 16:27:03
 
 ### 문제 설명
 
--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,34 +1,29 @@
 class Solution {
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
-        int top = 0;
-        int left = 0;
-        int right = n - 1;
-        int bottom = n - 1;
+        int top = 0, left = 0, right = n - 1, bottom = n - 1;
         int value = 1;
-        
-        while (value <= n * n) {
-            for (int i = left; i <= right; i++) {  // 상단
-                answer[top][i] = value++;
-            }
+
+        while (top <= bottom && left <= right) {
+            // 상단
+            for (int i = left; i <= right; i++) answer[top][i] = value++;
             top++;
-            
-            for (int i = top; i <= bottom; i++) {  // 우측
-                answer[i][right] = value++;
-            }
+            if (top > bottom) break;
+
+            // 우측
+            for (int i = top; i <= bottom; i++) answer[i][right] = value++;
             right--;
-            
-            for (int i = right; i >= left; i--) {  // 하단
-                answer[bottom][i] = value++;
-            }
+            if (left > right) break;
+
+            // 하단
+            for (int i = right; i >= left; i--) answer[bottom][i] = value++;
             bottom--;
-            
-            for (int i = bottom; i >= top; i--) {  // 좌측
-                answer[i][left] = value++;
-            }
+            if (top > bottom) break;
+
+            // 좌측
+            for (int i = bottom; i >= top; i--) answer[i][left] = value++;
             left++;
         }
-        
         return answer;
     }
 }
