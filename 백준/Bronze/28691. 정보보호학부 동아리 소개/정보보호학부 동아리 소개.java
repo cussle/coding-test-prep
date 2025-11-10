7bee7025eda2b3fe80471e4ed191a6fb74730486
cussle <109949453+cussle@users.noreply.github.com>
2025-11-10 22:07:08 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 108 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        switch (input) {
+            case "M":
+                System.out.println("MatKor");
+                break;
+
+            case "W":
+                System.out.println("WiCys");
+                break;
+
+            case "C":
+                System.out.println("CyKor");
+                break;
+
+            case "A":
+                System.out.println("AlKor");
+                break;
+            
+            default:
+                System.out.println("$clear");
+                break;
+        }
+    }
+}
