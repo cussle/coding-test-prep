aa48efdb74efa445894400c0930cf2563a820c86
cussle <109949453+cussle@users.noreply.github.com>
2025-11-12 15:13:53 +0900

    [Bronze III] Title: 특정 대문자를 소문자로 바꾸기, Time: 176 ms, Memory: 16276 KB -BaekjoonHub

--- 백준/Bronze/26040. 특정 대문자를 소문자로 바꾸기/특정 대문자를 소문자로 바꾸기.java
+++ 백준/Bronze/26040. 특정 대문자를 소문자로 바꾸기/특정 대문자를 소문자로 바꾸기.java
@@ -1,22 +1,21 @@
-import java.io.*;
-import java.util.*;
+    import java.io.*;
+    import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String a = br.readLine();
-        Set<Character> b = new HashSet<>();
+    public class Main {
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String a = br.readLine();
+            Set<Character> b = new HashSet<>();
 
-        for (char c : br.readLine().toCharArray()) {
-            b.add(c);
-        }
-
-        for (char c : a.toCharArray()) {
-            if (b.contains(c)) {
-                System.out.print(Character.toLowerCase(c));
-                continue;
+            for (char c : br.readLine().toCharArray()) {
+                b.add(c);
             }
-            System.out.print(c);
+
+            StringBuilder sb = new StringBuilder();
+            for (char c : a.toCharArray()) {
+                sb.append(b.contains(c) ? Character.toLowerCase(c) : c);
+            }
+
+            System.out.println(sb.toString());
         }
     }
-}
