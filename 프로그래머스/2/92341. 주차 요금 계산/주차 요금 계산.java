c940dabf49656e19961eb4fca4f30f80decc43b7
cussle <109949453+cussle@users.noreply.github.com>
2026-01-02 23:43:49 +0900

    [level 2] Title: 주차 요금 계산, Time: 6.94 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,47 +1,29 @@
 import java.util.*;
 
 class Solution {
-    static final boolean DEBUG = false;
     public int[] solution(int[] fees, String[] records) {
         StringTokenizer st;
-        Map<String, Boolean> wasInList = new HashMap<>();
-        Map<String, Integer> accTimes = new HashMap<>();
+        Map<String, Integer> accTimes = new TreeMap<>();
         
         for (String record : records) {
-            if (DEBUG) System.out.println(record + "에 대한 처리");
             st = new StringTokenizer(record);
-            while (st.hasMoreTokens()) {
-                int t = timeToInt(st.nextToken());
-                String carNum = st.nextToken();
-                boolean isOut = st.nextToken().length() == 3 ? true : false;
-                if (DEBUG) System.out.println(" = 변환된 시간: " + t);
-                
-                if (! isOut) {
-                    if (DEBUG) System.out.println(" = 입차");
-                    wasInList.put(carNum, true);
-                    accTimes.put(carNum, accTimes.getOrDefault(carNum, 0) - t);
-                    if (DEBUG) System.out.println(" = 누적 값 변경: " + accTimes.get(carNum));
-                    continue;
-                }
-                
-                if (DEBUG) System.out.println(" = 출차");
-                wasInList.put(carNum, false);
-                accTimes.put(carNum, accTimes.get(carNum) + t);
-                if (DEBUG) System.out.println(" = 누적 값 변경: " + accTimes.get(carNum));
+            int t = timeToInt(st.nextToken());
+            String carNum = st.nextToken();
+            boolean isOut = st.nextToken().length() == 3;
+
+            if (! isOut) {
+                accTimes.put(carNum, accTimes.getOrDefault(carNum, 0) - t);
+                continue;
             }
-            if (DEBUG) System.out.println(" = = = = = \n\n");
+
+            accTimes.put(carNum, accTimes.get(carNum) + t);
         }
         
-        List<String> keys = new ArrayList<>(wasInList.keySet());        
-        int[] answer = new int[keys.size()];
-        Collections.sort(keys);
-        
         int idx = 0;
-        for (String key : keys) {
-            if (wasInList.getOrDefault(key, false)) {
-                if (DEBUG) System.out.println(key + "에 대한 추가 처리");
+        int answer[] = new int[accTimes.size()];
+        for (String key : accTimes.keySet()) {
+            if (accTimes.get(key) < 1) {
                 accTimes.put(key, accTimes.get(key) + (23 * 60 + 59));
-                if (DEBUG) System.out.println(" = 누적 값 변경: " + accTimes.get(key));
             }
             
             int curTime = accTimes.getOrDefault(key, 0);
@@ -49,7 +31,6 @@ class Solution {
             if (curTime > fees[0]) {
                 answer[idx] += ((curTime - fees[0] + fees[2] - 1) / fees[2]) * fees[3];
             }
-            if (DEBUG) System.out.println(curTime);
             idx++;
         }
 
@@ -58,9 +39,7 @@ class Solution {
     
     private int timeToInt (String time) {
         int h = (time.charAt(0) - '0') * 10 + (time.charAt(1) - '0');
-        if (DEBUG) System.out.println(" = = = hour: " + h);
         int m = (time.charAt(3) - '0') * 10 + (time.charAt(4) - '0');
-        if (DEBUG) System.out.println(" = = = minute: " + m);
         return h * 60 + m;
     }
 }
