4b9b854fbe9553b8d2c1d85d18ce3d5c245cd4ec
cussle <109949453+cussle@users.noreply.github.com>
2026-01-14 22:16:00 +0900

    [Gold V] Title: LCS, Time: 140 ms, Memory: 19212 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        int len1 = s1.length();
+        int len2 = s2.length();
+
+        int[][] lengths = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i < len1 + 1; i++) {
+            for (int j = 1; j < len2 + 1; j++) {
+                char c1 = s1.charAt(i - 1);
+                char c2 = s2.charAt(j - 1);
+
+                if (c1 == c2) {
+                    lengths[i][j] = lengths[i - 1][j - 1] + 1;
+                } else {
+                    lengths[i][j] = Math.max(lengths[i - 1][j], lengths[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(lengths[len1][len2]);
+    }
+}
