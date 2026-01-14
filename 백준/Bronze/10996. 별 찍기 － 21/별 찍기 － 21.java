c424a5473e4fc8e24a79ce107b2ea3ee73bb831a
cussle <109949453+cussle@users.noreply.github.com>
2026-01-14 18:19:14 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 116 ms, Memory: 14424 KB -BaekjoonHub

--- 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.java
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.java
@@ -5,14 +5,17 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
 
         char[] stars = {'*', ' '};
         for (int i = 0; i < n * 2; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(stars[(i + j) % 2]);
+                sb.append(stars[(i + j) % 2]);
             }
-            System.out.println();
+            sb.append("\n");
         }
+
+        System.out.println(sb.toString());
     }
 }
