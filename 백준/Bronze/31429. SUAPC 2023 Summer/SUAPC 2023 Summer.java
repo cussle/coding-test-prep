05be4a5868f3df4ac36805f9b7f96ec09a4847da
cussle <109949453+cussle@users.noreply.github.com>
2026-01-21 22:05:11 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        String[] scripts = {
+            "12 1600",
+            "11 894",
+            "11 1327",
+            "10 1311",
+            "9 1004",
+            "9 1178",
+            "9 1357",
+            "8 837",
+            "7 1055",
+            "6 556",
+            "6 773"
+        };
+
+        System.out.println(scripts[n - 1]);
+    }
+}
