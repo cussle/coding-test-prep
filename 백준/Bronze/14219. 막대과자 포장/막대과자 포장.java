dd6bb94d015996a8e407134ac328b4399ea9f546
cussle <109949453+cussle@users.noreply.github.com>
2026-01-21 22:43:22 +0900

    [Bronze II] Title: 막대과자 포장, Time: 108 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/14219. 막대과자 포장/막대과자 포장.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int s = n * m;
+        s %= 6;
+        s %= 3;
+
+        System.out.println(s == 0 ? "YES" : "NO");
+    }
+}
