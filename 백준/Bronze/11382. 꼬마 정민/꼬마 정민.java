7a8e8d19ded04f04e4a44f67516263964e4f9fd1
cussle <109949453+cussle@users.noreply.github.com>
2026-01-27 13:21:04 +0900

    [Bronze V] Title: 꼬마 정민, Time: 92 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long answer = 0;
+
+        while (st.hasMoreTokens()) {
+            answer += Long.parseLong(st.nextToken());
+        }
+
+        System.out.println(answer);
+    }
+
+}
