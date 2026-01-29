9031876e54901d0b08977ab9b15dfc1843140053
cussle <109949453+cussle@users.noreply.github.com>
2026-01-29 15:13:17 +0900

    [Bronze V] Title: 덕후, Time: 92 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/34921. 덕후/덕후.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+        int p = 10 + 2 * (25 - a + t);
+
+        System.out.println(p > 0 ? p : 0);
+    }
+}
