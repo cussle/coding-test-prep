6c1919d1ed53ccfd3959befa12da44b3c084b2d4
cussle <109949453+cussle@users.noreply.github.com>
2026-01-30 17:26:21 +0900

    [Gold IV] Title: 팰린드롬?, Time: 716 ms, Memory: 218700 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -13,23 +13,33 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        boolean[][] dp = new boolean[n][n];
+
+        // 길이가 1인 경우
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        // 길이가 2인 경우
+        for (int i = 1; i < n; i++) {
+            dp[i - 1][i] = (arr[i - 1] == arr[i]);
+        }
+
+        // 길이가 3 이상인 경우
+        for (int length = 3; length <= n; length++) {
+            for (int end = length - 1; end < n; end++) {
+                int start = end - (length - 1);
+                dp[start][end] = (arr[start] == arr[end]) && dp[start + 1][end - 1];
+            }
+        }
+
         int m = Integer.parseInt(br.readLine());
         while (m --> 0) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken()) - 1; 
             int e = Integer.parseInt(st.nextToken()) - 1;
             
-            boolean result = true;
-            while (s <= e) {
-                if (arr[s] != arr[e]) {
-                    result = false;
-                    break;
-                }
-                s++;
-                e--;
-            }
-
-            sb.append(result ? 1 : 0).append("\n");
+            sb.append(dp[s][e] ? 1 : 0).append("\n");
         }
 
         System.out.println(sb.toString());        
