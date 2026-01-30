9dfe319e7f36e0adc3a2acf21852fbcb02e13947
cussle <109949453+cussle@users.noreply.github.com>
2026-01-30 15:21:36 +0900

    [Bronze IV] Title: 새천년관, Time: 88 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/33701. 새천년관/새천년관.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader br = new BufferedReader(new InputSreamReader(System.in));
+
+        System.out.println("nkugwan");
+    }
+}
