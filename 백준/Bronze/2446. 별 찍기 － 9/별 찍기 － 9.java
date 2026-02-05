5ca4fb9e6221fad915c9a0994c367c64be53fded
cussle <109949453+cussle@users.noreply.github.com>
2026-02-05 11:13:39 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 100 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine());
+        for (int i = n; i > 1; i--) {
+            sb.append(" ".repeat(n - i))
+              .append("*".repeat(i))
+              .append("*".repeat(i - 1))
+              .append("\n");
+        }
+        for (int i = 1; i <= n; i++) {
+            sb.append(" ".repeat(n - i))
+              .append("*".repeat(i))
+              .append("*".repeat(i - 1))
+              .append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
